8ff8c31db0a649f697c0432735a818c7c8643669
rotzanksiul <120349473+rotzanksiul@users.noreply.github.com>
2023-08-28 22:54:35 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+
+//create server
+const express = require('express')
+const app = express();
+const mysql = require('mysql')
+const cors = require('cors')
+
+//Make available to make request from the front end to the backend
+app.use(cors());
+//to read json files
+app.use(express.json());
+
+//making the connection to the sql database
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: process.env.SQL_PASSWORD,
+    database: "storagedgames"
+})
+
+//Requests
+
+//CREATE
+app.post('/create', (req, res) => {
+    const game = req.body.game
+    const genre = req.body.genre
+    const year = req.body.year
+    const platform = req.body.platform
+
+    db.query(
+        'INSERT INTO games ( game, genre, year, platform) VALUES (?,?,?,?)',
+        [game, genre, year, platform], 
+        (err, result) => {
+            if(err){
+                console.log(err);
+            } else {
+                console.log('values added')
+            }
+        });
+});
+
+//READ 
+app.get('/games', (req,res)=>{
+    
+    db.query("SELECT * FROM games", (err,result)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.send(result)
+            console.log('data received')
+        }
+    })
+})
+
+//UPDATE
+app.put('/games',(req, res)=>{
+    const id = req.body.id
+    const game = req.body.game
+    const genre = req.body.genre
+    const year = req.body.year
+    const platform = req.body.platform
+
+    db.query('UPDATE games SET game = ?, genre = ?, year = ?, platform = ? WHERE id =?', 
+    [game, genre, year, platform, id], 
+    ((err,result)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.send(result)
+            console.log('data updated')
+        }
+    }))
+})
+
+//DELETE
+app.delete('/games/:id',(req, res)=>{
+    const id = req.params.id
+
+    db.query('DELETE FROM games WHERE id = ?', id, 
+    (err, result)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.send(result)
+            console.log('data deleted')
+        }
+    })
+})
+
+const PORT = 3001
+
+// port to listen
+app.listen(process.env.PORT || PORT , () => {
+    console.log(`listening to port ${PORT}`)
+})
