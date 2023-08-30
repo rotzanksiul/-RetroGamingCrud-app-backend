1368eb9ae643ef16eed5f76717d1a0a3390bcd31
rotzanksiul <120349473+rotzanksiul@users.noreply.github.com>
2023-08-29 22:41:14 -0400

    .env changes DB_PORT removed

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    database: process.env.DB_NAME
 })
 
 //Requests
