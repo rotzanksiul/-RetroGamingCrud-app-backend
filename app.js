120a31124215c6bd74086285461886033bdcea79
rotzanksiul <120349473+rotzanksiul@users.noreply.github.com>
2023-08-29 21:36:00 -0400

    changed to createPool in Sql2 to manage better the connections

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 //create server
 const express = require('express')
 const app = express();
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const cors = require('cors')
 
 //Make available to make request from the front end to the backend
@@ -11,21 +11,16 @@ app.use(cors());
 //to read json files
 app.use(express.json());
 
+
 //making the connection to the sql database
-const db = mysql.createConnection({
+const pool  = mysql.createPool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectionLimit: 10
 })
 
-db.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL:', err);
-      return;
-    }
-    console.log('Connected to MySQL!');
-  });
 
 //Requests
 
@@ -36,7 +31,7 @@ app.post('/create', (req, res) => {
     const year = req.body.year
     const platform = req.body.platform
 
-    db.query(
+    pool.query(
         'INSERT INTO games ( game, genre, year, platform) VALUES (?,?,?,?)',
         [game, genre, year, platform], 
         (err, result) => {
@@ -51,7 +46,7 @@ app.post('/create', (req, res) => {
 //READ 
 app.get('/games', (req,res)=>{
     
-    db.query("SELECT * FROM games", (err,result)=>{
+    pool.query("SELECT * FROM games", (err,result)=>{
         if(err){
             console.log(err);
         } else {
@@ -69,7 +64,7 @@ app.put('/games',(req, res)=>{
     const year = req.body.year
     const platform = req.body.platform
 
-    db.query('UPDATE games SET game = ?, genre = ?, year = ?, platform = ? WHERE id =?', 
+    pool.query('UPDATE games SET game = ?, genre = ?, year = ?, platform = ? WHERE id =?', 
     [game, genre, year, platform, id], 
     ((err,result)=>{
         if(err){
@@ -85,7 +80,7 @@ app.put('/games',(req, res)=>{
 app.delete('/games/:id',(req, res)=>{
     const id = req.params.id
 
-    db.query('DELETE FROM games WHERE id = ?', id, 
+    pool.query('DELETE FROM games WHERE id = ?', id, 
     (err, result)=>{
         if(err){
             console.log(err);
