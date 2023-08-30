b8331f9b2cecd7bc9052368a8ed3aa11fd2b4757
rotzanksiul <120349473+rotzanksiul@users.noreply.github.com>
2023-08-29 23:11:39 -0400

    .env changes to DB_PORT

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 })
 
 //Requests
